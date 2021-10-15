5e4e58c32ace89a1de30ad34829b53a9d1c39fa2
Emilio Soto <emiliosoto@MacBook-Pro-de-Emilio.local>
2021-10-14 20:38:56 -0600

    Co-authored-by: Alexis alexis_gavriel@hotmail.com

---
+++ include/memory.h
@@ -0,0 +1,14 @@
+typedef struct Registers{
+  unsigned char A;
+  unsigned char B;
+  unsigned char C;
+  unsigned char D;
+  unsigned char E;
+  unsigned char F;
+  unsigned char H;
+  unsigned char L;
+  unsigned short PC;
+  unsigned short SP;
+} Registers;
+
+
